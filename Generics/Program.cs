6afb32de6f0c2cd423013c70a3c6ef7566832abc
KajalShendage <shendagekajal123@gmail.com>
2022-09-10 20:55:48 +0530

    [Pramod] Add: Write Print method to print Array

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Welcome to Generics Demo Program!");
+﻿namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArr = { 1, 2, 3, 4, 5 };
+            double[] doubleArr = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+            char[] charArr = { 'a', 'b', };
+            PrintIntArr(intArr);
+            PrintDoubleArr(doubleArr);
+            PrintCharArr(charArr);
+        }
+        static void PrintIntArr(int[] arr)
+        {
+            foreach (var data in arr)
+            {
+                Console.WriteLine(data); ;
+            }
+        }
+        static void PrintDoubleArr(double[] arr)
+        {
+            foreach (var data in arr)
+            {
+                Console.WriteLine(data); ;
+            }
+        }
+        static void PrintCharArr(char[] arr)
+        {
+            foreach (var data in arr)
+            {
+                Console.WriteLine(data); ;
+            }
+        }
+    }
+}
